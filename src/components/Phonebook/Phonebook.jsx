880c7ca422bdd70b95f50c88771afdf616f2fb29
Stanislav2022 <tsv5@ukr.net>
2022-09-28 23:08:27 +0300

    1

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { Component } from 'react'
+import css from "./Phonebook.module.css";
+import { nanoid } from 'nanoid'
 
-export default function Phonebook() {
-  return (
-    <div>Phonebook</div>
-  )
+export default class Phonebook extends Component {
+    state = {
+        contacts: [],
+        name: ''
+    };
+    handleName = (e) => {
+        this.setState({ title: e.target.value });
+    }
+    
+  render() {
+    return (
+        <>
+            <div>
+                <h2>Phonebook</h2>
+                <div className={css.form}>
+                    <label htmlFor="">Name</label>
+                    <input className={css.input} type="text" value={this.state.name} onChange={this.handleName} />
+                    <button>Add contact</button>
+                </div>
+            </div>
+            <div>
+                <h2>Contacts</h2>
+                <ul>
+                    <li>Rosie</li>
+                    <li>Hermione</li>
+                    <li>Eden</li>
+                </ul>
+
+            </div>
+       </>
+    )
+  }
 }
