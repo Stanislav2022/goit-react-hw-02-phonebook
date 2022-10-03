6e999930b444c0ff649d5bfbf8a6531b5e3f1129
Stanislav2022 <tsv5@ukr.net>
2022-10-03 22:43:37 +0300

    1

--- src/components/Phonebook/ContactList/ContactList.jsx
+++ src/components/Phonebook/ContactList/ContactList.jsx
@@ -1,5 +1,8 @@
-export default function ContactList() {
+export default function ContactList({ items }) {
+    const element = items.map(({name, number, id}) => {
+        return <li key={id}>{name}: {number}</li>
+    })
   return (
-    <div>ContactList</div>
+    <ul>{element}</ul>
   )
 }
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,24 +1,31 @@
 import { Component } from 'react'
 import css from "./Phonebook.module.css";
-// import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid'
 import ContactForm from "./ContactForm/ContactForm"
+import ContactList from './ContactList/ContactList';
 
 export default class Phonebook extends Component {
     state = {
-        contacts: [],
+        contacts: [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
         filter: ''
     };
 
     addContats = (data) => {
         this.setState((prev) => {
+            const newData = { id: nanoid(), ...data}
             return {
-                contacts: [...prev.contacts, data]
+                contacts: [...prev.contacts, newData]
             }
         })
     };
     
     render() {
         const { addContats } = this;
+        const { contacts } = this.state;
     return (
         <>
             <div className={css.form}>
@@ -27,11 +34,8 @@ export default class Phonebook extends Component {
             </div>
             <div>
                 <h2>Contacts</h2>
-                <ul>
-                    <li>Rosie</li>
-                    <li>Hermione</li>
-                    <li>Eden</li>
-                </ul>
+                <ContactList items={ contacts} />
+
 
             </div>
        </>
